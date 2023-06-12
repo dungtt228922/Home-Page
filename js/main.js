85a5089f11f39e0e8bbcf600d432ba000d518e33
dungtt228922 <dungtt@regit-technology.com>
2023-06-12 18:13:37 +0700

    amimation

--- js/main.js
+++ js/main.js
@@ -3,27 +3,24 @@ let menu_expand = document.getElementById('hidden-menu-expand')
 let header_mb = document.getElementById('nav-mb')
 let header_pc = document.getElementById('nav-pc')
 
-menu_expand_icon.addEventListener('click', () => {
-    if (menu_expand.style.display !== 'none') {
-        menu_expand.style.display = 'none'
-    } else {
-        menu_expand.style.display = 'flex'
-    }
-})
-
 window.onscroll = function(e) {
     if (this.oldScroll < this.scrollY) {
-        header_mb.style.display = 'none'
-        header_pc.style.display = 'none'
-        header_pc.classList.remove ('scrolled')
-        if (menu_expand.style.display !== 'none') {
-            menu_expand.style.display = 'none'
-        }
+        header_pc.classList.remove('scrolled')
+        header_mb.classList.remove('scrolled')
+
     } else {
         console.log('Lmao')
-        header_mb.style.display = 'flex'
-        header_pc.style.display = 'flex'
-        header_pc.classList.add ('scrolled')
+        header_pc.classList.add('scrolled')
+        header_mb.classList.add('scrolled')
     }
     this.oldScroll = this.scrollY
   }
+
+  menu_expand_icon.addEventListener('click', function () {
+    if (menu_expand.classList.contains('scrolled')) {
+        menu_expand.classList.remove('scrolled')
+    } else {
+        menu_expand.classList.add('scrolled')
+    }
+})
+
