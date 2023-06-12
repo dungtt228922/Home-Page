f3a720124da1042fd29a101febfee563ca299859
dungtt228922 <dungtt@regit-technology.com>
2023-06-12 16:15:56 +0700

    fix:navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let menu_expand_icon = document.querySelector('#menu_expand_icon')
-let menu_expand = document.getElementById('hidden_menu_expand')
-let header_mb = document.getElementById('nav_mb')
+let menu_expand_icon = document.querySelector('#menu-expand-icon')
+let menu_expand = document.getElementById('hidden-menu-expand')
+let header_mb = document.getElementById('nav-mb')
+let header_pc = document.getElementById('nav-pc')
 
 menu_expand_icon.addEventListener('click', () => {
     if (menu_expand.style.display !== 'none') {
@@ -13,11 +14,16 @@ menu_expand_icon.addEventListener('click', () => {
 window.onscroll = function(e) {
     if (this.oldScroll < this.scrollY) {
         header_mb.style.display = 'none'
+        header_pc.style.display = 'none'
+        header_pc.classList.remove ('scrolled')
         if (menu_expand.style.display !== 'none') {
             menu_expand.style.display = 'none'
         }
     } else {
+        console.log('Lmao')
         header_mb.style.display = 'flex'
+        header_pc.style.display = 'flex'
+        header_pc.classList.add ('scrolled')
     }
     this.oldScroll = this.scrollY
   }
